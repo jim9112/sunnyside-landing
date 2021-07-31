3e8ab50b026503cce6fd5aa75b424daaf144e40e
jim9112 <jim9112@gmail.com>
2021-07-31 15:58:10 -0400

    configure tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-600">Sunnyside Landing Page</h1>
+      <h1 className="text-primary-dark-cyan">Sunnyside Landing Page</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      'primary-soft-red': 'hsl(7, 99%, 70%)',
+      'primary-yellow': 'hsl(51, 100%, 49%)',
+      'primary-dark-cyan': 'hsl(167, 40%, 24%)',
+      'primary-dark-blue': 'hsl(198, 62%, 26%)',
+      'primary-moderate-cyan': 'hsl(168, 34%, 41%)',
+      'neutral-very-dark-blue': 'hsl(212, 27%, 19%)',
+      'neutral-very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+      'neutral-dark-greyish-blue': 'hsl(232, 10%, 55%)',
+      'neutral-greyish-blue': 'hsl(210, 4%, 67%)',
+      'neutral-white': 'hsl(0, 0%, 100%)',
+    },
+    fontFamily: {
+      barlow: ['Barlow', 'sans-serif'],
+      fraunces: ['Fraunces', 'serif'],
+    },
     extend: {},
   },
   variants: {
