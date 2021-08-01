fbed06b20e11023b12bbf71193606b5170fc1a49
jim9112 <jim9112@gmail.com>
2021-08-01 15:53:49 -0400

    services section complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
 import MainContent from '../components/MainContent';
+import Services from '../components/Services';
 
 export default function Home() {
   return (
     <div>
       <Header />
       <MainContent />
+      <Services />
     </div>
   );
 }
