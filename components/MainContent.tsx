40640a6eea0f47e802dd9af569ecac8fe9291fbf
jim9112 <jim9112@gmail.com>
2021-07-31 20:06:31 -0400

    content block two mobile

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -2,6 +2,17 @@ const MainContent = () => {
   return (
     <div>
       <div className="bg-mobile-egg h-80 w-full bg-no-repeat bg-cover bg-center"></div>
+      <div className="px-6 py-16 text-center">
+        <h1 className="font-fraunces text-3xl mb-6">Transform your brand</h1>
+        <p className="text-neutral-dark-greyish-blue font-barlow text-lg mb-8">
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </p>
+        <a className="font-fraunces text-neutral-very-dark-blue" href="">
+          LEARN MORE
+        </a>
+      </div>
     </div>
   );
 };
