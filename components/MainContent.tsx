9ed12410c6e8ec473094c6482d553e98a9d733b0
jim9112 <jim9112@gmail.com>
2021-08-02 10:30:45 -0400

    main content desktop styling

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -1,10 +1,10 @@
 const MainContent = () => {
   return (
-    <div>
+    <div className="md:grid md:grid-cols-2">
       {/* egg image (transform) */}
-      <div className="bg-mobile-egg h-80 w-full bg-no-repeat bg-cover bg-center"></div>
+      <div className="md:order-2 bg-mobile-egg md:bg-desktop-egg md:h-full h-80 w-full bg-no-repeat bg-cover bg-center"></div>
       {/* transform description */}
-      <div className="px-6 py-16 text-center">
+      <div className="md:order-1 px-6 py-16 md:py-36 lg:px-40 text-center">
         <h1 className="font-fraunces text-3xl mb-6">Transform your brand</h1>
         <p className="text-neutral-dark-greyish-blue font-barlow text-lg mb-8">
           We are a full-service creative agency specializing in helping brands
@@ -16,9 +16,9 @@ const MainContent = () => {
         </a>
       </div>
       {/* stand out image */}
-      <div className="bg-mobile-glass h-80 w-full bg-no-repeat bg-cover bg-center"></div>
+      <div className="md:order-3 bg-mobile-glass md:bg-desktop-glass md:h-full h-80 w-full bg-no-repeat bg-cover bg-center"></div>
       {/* stand out description */}
-      <div className="px-6 py-16 text-center">
+      <div className="md:order-4 px-6 py-16 md:py-36 lg:px-40 text-center">
         <h1 className="font-fraunces text-3xl mb-6">
           Stand out to the right audience
         </h1>
